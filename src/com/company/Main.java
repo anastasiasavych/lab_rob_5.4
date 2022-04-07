c7618e209a0011484595a5aae256507a1032b5eb
Анастасія Савич <anastasiasavych@MacBook-Pro-Anastasia.local>
2022-04-07 11:06:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Батько: Савич Ігор Михайлович, 1973 року народження, приватний підприємець");
+        System.out.println("Мати: Савич Алла Анатоліївна, 1973 року народження, майстер зміни ЛКФ ");
+        System.out.println("Брат: Савич Юрій Ігорович, 2008 року народження, учень");
+
+    }
+}
